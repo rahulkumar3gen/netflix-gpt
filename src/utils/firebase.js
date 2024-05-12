f8c583e50a43cd3a4862e2069bb11affeabbaae8
rahulkumar3gen <101503039+rahulkumar3gen@users.noreply.github.com>
2024-05-12 11:16:46 +0530

    Update firebase.js
    Add your firebase API key here

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,13 +9,8 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDmLH1ZyZwxpS1EUL0ceXuSUXt-tT1i9eU",
-  authDomain: "netflixgpt-b6d2e.firebaseapp.com",
-  projectId: "netflixgpt-b6d2e",
-  storageBucket: "netflixgpt-b6d2e.appspot.com",
-  messagingSenderId: "1048333018676",
-  appId: "1:1048333018676:web:74221b94d4b9e516760c61",
-  measurementId: "G-5M00W35B1Y",
+  
+
 };
 
 // Initialize Firebase
